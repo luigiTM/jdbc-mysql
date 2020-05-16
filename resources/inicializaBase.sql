47bc0d38831071a8cc4182fb5db803cf485ef860
Luigi <luigitosin@gmail.com>
2020-05-16 19:08:52 -0300

    Inclusão do script SQL

---
+++ resources/inicializaBase.sql
@@ -0,0 +1,59 @@
+CREATE TABLE departamento (
+
+  Id int(11) NOT NULL AUTO_INCREMENT,
+
+  Nome varchar(60) DEFAULT NULL,
+
+  PRIMARY KEY (Id)
+
+);
+
+
+
+CREATE TABLE vendedor (
+
+  Id int(11) NOT NULL AUTO_INCREMENT,
+
+  Nome varchar(60) NOT NULL,
+
+  Email varchar(100) NOT NULL,
+
+  DataNascimento datetime NOT NULL,
+
+  SalarioBase double NOT NULL,
+
+  IdDepartamento int(11) NOT NULL,
+
+  PRIMARY KEY (Id),
+
+  FOREIGN KEY (IdDepartamento) REFERENCES departamento (id)
+
+);
+
+
+
+INSERT INTO departamento (Nome) VALUES 
+
+  ('Computers'),
+
+  ('Electronics'),
+
+  ('Fashion'),
+
+  ('Books');
+
+
+
+INSERT INTO vendedor (Nome, Email, DataNascimento, SalarioBase, IdDepartamento) VALUES 
+
+  ('Bob Brown','bob@gmail.com','1998-04-21 00:00:00',1000,1),
+
+  ('Maria Green','maria@gmail.com','1979-12-31 00:00:00',3500,2),
+
+  ('Alex Grey','alex@gmail.com','1988-01-15 00:00:00',2200,1),
+
+  ('Martha Red','martha@gmail.com','1993-11-30 00:00:00',3000,4),
+
+  ('Donald Blue','donald@gmail.com','2000-01-09 00:00:00',4000,3),
+
+  ('Alex Pink','bob@gmail.com','1997-03-04 00:00:00',3000,2);
